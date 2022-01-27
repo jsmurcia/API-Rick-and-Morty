3a4d627a75ec013fbd86b024170c2c62bc8e1669
Sebastian Murcia <smurcia162@gmail.com>
2022-01-27 12:25:52 -0500

    consumo de api

---
+++ src/Componentes/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div>
+      <a>
+        <img src='http://assets.stickpng.com/images/58f37720a4fa116215a9240f.png' alt='IMG LOGO'/>
+      </a>
+    </div>);
+};
+
+export default Logo;
